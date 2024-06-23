b8867c1c056d0403db4139369f883e6fc1943a00
Samir Hossain <samirhossain924@gmail.com>
2024-06-23 21:47:59 +0200

    2nd commit

--- resources/views/admin/admin_dashboard.blade.php
+++ resources/views/admin/admin_dashboard.blade.php
@@ -7,21 +7,7 @@
 	<!--wrapper-->
 	<div class="wrapper">
 		<!--sidebar wrapper -->
-		<div class="sidebar-wrapper" data-simplebar="true">
-			<div class="sidebar-header">
-				<div>
-					<img src="{{ asset('backend/assets/images/logo-icon.png')}}" class="logo-icon" alt="logo icon">
-				</div>
-				<div>
-					<h4 class="logo-text">Rukada</h4>
-				</div>
-				<div class="toggle-icon ms-auto"><i class='bx bx-arrow-to-left'></i>
-				</div>
-			</div>
-			<!--navigation-->
-			@include('admin.body.navigation')
-			<!--end navigation-->
-		</div>
+		@include('admin.body.sidebar')
 		<!--end sidebar wrapper -->
 		<!--start header -->
 		@include('admin.body.header')
--- resources/views/admin/body/js.blade.php
+++ resources/views/admin/body/js.blade.php
@@ -57,4 +57,14 @@
 	  selector: '#mytextarea'
 	});
 </script>
+
+
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+<script>
+	$(document).ready(function () {
+		$('.menu-title').on('click', function () {
+			$(this).next('.submenu').slideToggle();
+		});
+	});
+</script>
 <!--Datatable-->
